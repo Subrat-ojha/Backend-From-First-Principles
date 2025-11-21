049627d8bf86abe226263100d8f1cb39227d2917
Subrat-ojha <iamsubratojha@gmail.com>
2025-11-21 23:19:16 +0530

    post call added

--- src/main/java/com/backendd/controller/HomeController.java
+++ src/main/java/com/backendd/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.backendd.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class HomeController {
 	@GetMapping("v1/home")
 	public String getHome() {
 		return "It is first backend return";
+	}
+	@PostMapping("v1/post")
+	public String getPost() {
+		return "It is first backend Post call";
 	}
 }
