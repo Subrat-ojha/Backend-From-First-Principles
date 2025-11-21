2454656cfacbd1ee4aa81a7d568c18cc05da0393
Subrat-ojha <iamsubratojha@gmail.com>
2025-11-21 23:10:03 +0530

    HomeController added for first api request

---
+++ src/main/java/com/backendd/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.backendd.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	
+	@GetMapping("v1/home")
+	public String getHome() {
+		return "It is first backend return";
+	}
+}
