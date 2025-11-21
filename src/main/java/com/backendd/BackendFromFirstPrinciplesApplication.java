875732c2fba85f31cf63bcc165b2e8a09b4f2efc
Subrat-ojha <iamsubratojha@gmail.com>
2025-11-21 22:41:27 +0530

    learning backend

---
+++ src/main/java/com/backendd/BackendFromFirstPrinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.backendd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFromFirstPrinciplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFromFirstPrinciplesApplication.class, args);
+	}
+
+}
